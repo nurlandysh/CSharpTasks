648f73e5d117b4d77b6d37c4b6f5e2b8506dc28f
Шаймарданов Ильфат <ilfatsh@Sajmardanovs-MacBook-Pro.local>
2022-10-07 10:59:10 +0300

    Task 2 solved

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿// 
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.Clear();
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите первое число: ");
+int x = int.Parse(Console.ReadLine()!);
 
-int sqr = number*number;
-Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+Console.WriteLine("Введите второе число: ");
+int y = int.Parse(Console.ReadLine()!);
 
+if (x > y)
+
+{
+    Console.WriteLine("Число {x} больше числа {y}");
+}
+else
+{
+    Console.WriteLine("Число {x} меньше числа {y}");
+}
