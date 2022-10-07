61c3d1f8cb410519ceb35dd8955a849f05d56811
Шаймарданов Ильфат <ilfatsh@Sajmardanovs-MacBook-Pro.local>
2022-10-07 10:45:17 +0300

    Task 0 solved

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// 
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sqr = number*number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+
